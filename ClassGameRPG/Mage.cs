efb91621ec8d738fba82e858c8a6725d8d103a9d
DenisPanfilov0 <denispanfilov119@gmail.com>
2023-03-05 20:41:18 +0300

    Add Current Health Enemy

--- ClassGameRPG/Mage.cs
+++ ClassGameRPG/Mage.cs
@@ -9,11 +9,9 @@ public class Mage : Unit
     public override int DamagePlayer { get; set; } = 100;
     public override int HealthPlayer { get; set; } = 70;
     private double ArmorPenetration { get; set; } = 40;
-    private double DamageEnemy { get; set; } = 40;
-    private double EnemyResistance { get; set; } = 50;
-    
+
     public override void Attack()
     {
-        Console.WriteLine("Mage deals damage = " + (DamagePlayer - (EnemyResistance - ArmorPenetration)) + ".   And received damage = " + DamageEnemy);
+        Console.WriteLine("Mage deals damage = " + (DamagePlayer - (EnemyResistance - ArmorPenetration)) + ".   And received damage = " + DamageEnemy + ".  Current Health Enemy = " + (CurrentHealth - DamagePlayer));
     }
 }
--- ClassGameRPG/Unit.cs
+++ ClassGameRPG/Unit.cs
@@ -5,8 +5,9 @@ public abstract class Unit
     
     public abstract int DamagePlayer { get; set; } 
     public abstract int HealthPlayer { get; set; }
-    public double DamageEnemy { get; set; } 
-    public double EnemyResistance { get; set; }
+    public double DamageEnemy { get; set; } = 40;
+    public double EnemyResistance { get; set; } = 50;
+    public double CurrentHealth = 200;
     
     
     public abstract void Attack();
